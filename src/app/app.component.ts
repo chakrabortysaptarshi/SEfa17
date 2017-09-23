f8c6727265fec61bbfd9c22a0a61fa8d83b5c05a
chakrabortysaptarshi <sssaptarshii@gmail.com>
2017-09-23 12:07:30 -0400

    Revert "Pulling backend structure."

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +6,5 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Under Construction';
-  myData: Array<any> = [];
-  res: Object;
-
-    constructor(private http: HttpClient) {
-
-      this.http.get('https://jsonplaceholder.typicode.com/photos')
-      .subscribe(res => {
-        this.res = res;
-        [].push.apply(this.myData, res);
-      });
-    }
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
-import { MaterialModule } from './custom-material/custom-material.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    MaterialModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
